e3513e90b8f16286645bd5b258d498c82355f6d0
lenabezgodova <lena.bezgodova@gmail.com>
2021-08-26 11:59:27 +0200

    new Function - distance
    Function - Distance between two coordinates

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,8 +1,22 @@
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
 public class MyFirstProgram {
 
     public static void main(String[] args){
         System.out.println("Hello world");
-        System.out.println("minor changes - update");
+
+
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(2, 2);
+
+        System.out.println("Distance between two coordinates: " +  distance(p1, p2));
+    }
+
+
+    public static double distance(Point p1, Point p2){
+        return sqrt(pow((p2.coorX - p1.coorX),2) + pow((p2.coorY - p1.coorY), 2));
+
     }
 }
 
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,12 @@
+public class Point {
+
+    double coorX;
+    double coorY;
+
+    public Point(double coorX, double coorY){
+        this.coorX = coorX;
+        this.coorY = coorY;
+    }
+
+
+}
