486c6a35da43c7cb1ef844f8fd1b70972540a66d
lenabezgodova <lena.bezgodova@gmail.com>
2021-10-05 15:31:48 +0200

    12. Генератор тестовых данных для контактов
    test - User creation
    can create in two formats:
     - CSV
     - XML

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/UserDataGeneration.java
@@ -0,0 +1,82 @@
+package ru.stqa.pft.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
+import ru.stqa.pft.addressbook.model.UserData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDataGeneration {
+
+    @Parameter(names = "-c", description = "User count")
+    public int count;
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+    @Parameter(names = "-d", description = "Data format")
+    public String format;
+
+    public static void main(String[] args) throws IOException {
+        UserDataGeneration generator = new UserDataGeneration();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<UserData> users = generateUsers(count);
+        if (format.equals("csv")){
+            saveAsCsv(users, new File(file));
+        } else if (format.equals("xml")) {
+            saveAsXml(users, new File(file));
+        } else {
+            System.out.println("Unrecognized format: " + format);
+        }
+
+    }
+
+    private void saveAsXml(List<UserData> users, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(UserData.class);
+        String xml = xstream.toXML(users);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+    private void saveAsCsv (List<UserData> users, File file) throws IOException { //нужно этот список сохранить в файл
+        Writer writer = new FileWriter(file);
+
+        for(UserData user: users){
+            writer.write(String.format("%s;%s;%s;%s;%s\n", user.getFirstName(), user.getLastName(), user.getMiddleName(), user.getGroup(), user.getWorkPhone()));
+        }
+        writer.close();
+    }
+
+    private List<UserData> generateUsers (int count){
+        List<UserData> users = new ArrayList<UserData>();
+
+        for (int i = 0; i < count; i++){
+            users.add(new UserData().withFirstName(String.format("test %s", i))
+                    .withLastName(String.format("header %s", i))
+                    .withMiddleName(String.format("footer %s", i))
+                    .withGroup("test1")
+                    .withWorkPhone(String.format("+7980(100)565%s", i)));
+        }
+        return users;
+    }
+
+
+
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/UserData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/UserData.java
@@ -1,10 +1,15 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.io.File;
 import java.util.Objects;
 
+@XStreamAlias("users")
 public class UserData {
 
+    @XStreamOmitField
     private int id = Integer.MAX_VALUE;
     private String firstName;
     private String middleName;
