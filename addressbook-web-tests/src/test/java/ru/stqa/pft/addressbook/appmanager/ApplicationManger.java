a99e28555432f54a9b60d8c4009be23ddc2719ae
lenabezgodova <lena.bezgodova@gmail.com>
2021-10-28 14:19:10 +0200

    Remote server
    Реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManger.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManger.java
@@ -7,10 +7,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -37,17 +40,24 @@ public class ApplicationManger {
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
         dbHelper = new DbHelper();
+        if ("".equals(properties.getProperty("selenium.server"))){
+            if (browser.equals(BrowserType.FIREFOX)){
+                System.setProperty("webdriver.gecko.driver", "C:\\Users\\lenab\\IdeaProjects\\education_qa-java\\addressbook-web-tests\\drivers\\geckodriver.exe");
+                driver = new FirefoxDriver();
 
-        if (browser.equals(BrowserType.FIREFOX)){
-            System.setProperty("webdriver.gecko.driver", "C:\\Users\\lenab\\IdeaProjects\\education_qa-java\\addressbook-web-tests\\drivers\\geckodriver.exe");
-            driver = new FirefoxDriver();
-
-        } else  if (browser.equals(BrowserType.CHROME)){
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\lenab\\IdeaProjects\\education_qa-java\\addressbook-web-tests\\drivers\\chromedriver.exe");
-            driver = new ChromeDriver();
-        } else if (browser.equals(BrowserType.EDGE)){
-            driver = new EdgeDriver();
+            } else  if (browser.equals(BrowserType.CHROME)){
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\lenab\\IdeaProjects\\education_qa-java\\addressbook-web-tests\\drivers\\chromedriver.exe");
+                driver = new ChromeDriver();
+            } else if (browser.equals(BrowserType.EDGE)){
+                driver = new EdgeDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            driver = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
+
+
 
 
         Thread.sleep(5000);
